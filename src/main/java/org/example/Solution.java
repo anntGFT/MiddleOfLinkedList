2f6bafe88bfd087a7dc01f10b5fb01f202d27ac5
annt <annt@gft.com>
2022-11-22 10:18:41 +0000

    Done with while loop

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,2 +1,22 @@
-package org.example;public class Solution {
+package org.example;
+
+public class Solution {
+
+    public Solution(){
+
+    }
+
+    public ListNode middleNode(ListNode head) {
+
+        if (head == null || head.next == null) return head;
+
+        ListNode first = head;
+        ListNode second = head;
+
+        while (second != null && second.next != null) {
+            first = first.next;
+            second = second.next.next;
+        }
+        return first;
+    }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,2 +1,55 @@
-package PACKAGE_NAME;public class Solution {
+import org.example.ListNode;
+import org.example.Solution;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SolutionTest {
+
+    Solution solution;
+    ListNode listNode;
+    ListNode result;
+    ListNode next;
+
+    @BeforeAll
+    void setup(){
+        solution = new Solution();
+    }
+    @Test
+    public void middleOfOneNumber(){
+        listNode = new ListNode(4);
+        result = new ListNode(4);
+
+        assertEquals(result.getVal(), solution.middleNode(listNode).getVal());
+    }
+
+    @Test
+    public void middleOfTwoNumber(){
+        next = new ListNode(3);
+        listNode = new ListNode(4,next);
+        result = new ListNode(3);
+
+        assertEquals(result.getVal(), solution.middleNode(listNode).getVal());
+    }
+
+    @Test
+    public void middleOfFiveNumber(){
+        next = new ListNode(345);
+        listNode = new ListNode(12,next);
+        result = new ListNode(345);
+
+        assertEquals(result.getVal(), solution.middleNode(listNode).getVal());
+    }
+
+    @Test
+    public void middleOfSixNumber(){
+        next = new ListNode(456);
+        listNode = new ListNode(123,next);
+        result = new ListNode(456);
+
+        assertEquals(result.getVal(), solution.middleNode(listNode).getVal());
+    }
 }
